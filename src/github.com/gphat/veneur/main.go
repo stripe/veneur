7e38a57863538f567f1c9baab786a97bb602b88a
Cory G Watson <gphat@onemogin.com>
2016-04-02 16:08:46 -0500

    Stores correctly, but tags don't work.

--- src/github.com/gphat/veneur/main.go
+++ src/github.com/gphat/veneur/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"hash/fnv"
+	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
+	"time"
 )
 
 var (
@@ -23,7 +29,6 @@ func main() {
 
 	// Start the dispatcher.
 	log.Println("Starting workers")
-
 	workers := make([]Worker, *NWorkers)
 	for i := 0; i < *NWorkers; i++ {
 		worker := NewWorker(i+1, *Interval)
@@ -31,9 +36,7 @@ func main() {
 		workers[i] = worker
 	}
 
-	// StartDispatcher(*NWorkers, WorkQueue)
-
-	// Start the HTTP server!
+	// Start the UDP server!
 	log.Println("UDP server listening on", *UDPAddr)
 	ServerAddr, err := net.ResolveUDPAddr("udp", ":8125")
 	if err != nil {
@@ -47,13 +50,25 @@ func main() {
 
 	defer ServerConn.Close()
 
+	ticker := time.NewTicker(time.Duration(*Interval) * time.Second)
+	go func() {
+		for t := range ticker.C {
+			metrics := make([][]PostMetric, *NWorkers)
+			for i, w := range workers {
+				log.Printf("Flushing worker %d at %v", i, t)
+				metrics = append(metrics, w.Flush())
+			}
+			flush(metrics)
+		}
+	}()
+
 	// TODO More than 4096?
 	buf := make([]byte, 4096)
 
 	for {
 		n, _, err := ServerConn.ReadFromUDP(buf)
 		if err != nil {
-			log.Println("Error: ", err)
+			log.Println("Error reading from UDP: ", err)
 		}
 
 		// We could maybe free up the Read above by moving
@@ -73,5 +88,44 @@ func main() {
 		// to the work queue. Note that if the queue is full, we'll block
 		// here.
 		workers[index].WorkChan <- *m
+	}
+}
+
+// Flush takes the slices of metrics, combines then and marshals them to json
+// for posting to Datadog.
+func flush(postMetrics [][]PostMetric) {
+	totalCount := 0
+	var finalMetrics []PostMetric
+	// TODO This seems very inefficient
+	for _, metrics := range postMetrics {
+		totalCount += len(metrics)
+		finalMetrics = append(finalMetrics, metrics...)
+	}
+	// Check to see if we have anything to do
+	if totalCount > 0 {
+		// Make a metric for how many metrics we metriced (be sure to add one to the total for it!)
+		finalMetrics = append(
+			finalMetrics,
+			// TODO Is this the right type?
+			NewPostMetric("veneur.stats.metrics_posted", float32(totalCount+1), "", "counter", *Interval),
+		)
+		postJSON, _ := json.MarshalIndent(map[string][]PostMetric{
+			"series": finalMetrics,
+		}, "", "  ")
+
+		resp, err := http.Post("https://app.datadoghq.com/api/v1/series?api_key=baefa227ac0f2182c26d3b22cdc7ce5d", "application/json", bytes.NewBuffer(postJSON))
+		if err != nil {
+			log.Printf("Error posting: %q", err)
+		}
+		defer resp.Body.Close()
+		fmt.Println("Response Status:", resp.Status)
+		fmt.Println("Response Headers:", resp.Header)
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("Error reading response body: %q", err)
+		}
+		fmt.Printf("Response: %q", body)
+	} else {
+		log.Println("Nothing to flush.")
 	}
 }
--- src/github.com/gphat/veneur/worker.go
+++ src/github.com/gphat/veneur/worker.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"sync"
@@ -9,6 +8,19 @@ import (
 
 	"github.com/rcrowley/go-metrics"
 )
+
+type Worker struct {
+	id            int
+	flushInterval int
+	percentiles   []float64
+	WorkChan      chan Metric
+	FlushChan     chan bool
+	QuitChan      chan bool
+	counters      map[Metric]int32
+	gauges        map[Metric]int32
+	histograms    map[Metric]metrics.Histogram
+	mutex         *sync.Mutex
+}
 
 // NewWorker creates, and returns a new Worker object. Its only argument
 // is a channel that the worker will receive work from.
@@ -19,37 +31,33 @@ func NewWorker(id int, flushInterval int) Worker {
 		flushInterval: flushInterval,
 		percentiles:   []float64{0.5, 0.75, 0.99},
 		WorkChan:      make(chan Metric, 100),
-		QuitChan:      make(chan bool)}
+		FlushChan:     make(chan bool),
+		QuitChan:      make(chan bool),
+		mutex:         &sync.Mutex{},
+		counters:      make(map[Metric]int32),
+		gauges:        make(map[Metric]int32),
+		histograms:    make(map[Metric]metrics.Histogram),
+	}
 
 	return worker
 }
 
-type Worker struct {
-	id            int
-	flushInterval int
-	percentiles   []float64
-	WorkChan      chan Metric
-	QuitChan      chan bool
-}
-
 type PostMetric struct {
-	Name       string  `json:"name"`
-	Timestamp  int64   `json:"timestamp"`
-	Value      float32 `json:"value"`
-	Tags       string  `json:"tags"`
-	MetricType string  `json:"metric_type"`
-	// hostname
+	Name       string        `json:"metric"`
+	Value      [1][2]float32 `json:"points"`
+	Tags       string        `json:"tags"`
+	MetricType string        `json:"type"`
+	Hostname   string        `json:"host"`
 	// devicename
 	Interval int32 `json:"interval"`
 }
 
 func NewPostMetric(name string, v float32, tags string, metricType string, interval int) PostMetric {
 	return PostMetric{
-		Name:      name,
-		Timestamp: time.Now().Unix(),
-		Value:     float32(v), // TODO FIX ME!
-		Tags:      tags,
-		// hostname
+		Name:     name,
+		Value:    [1][2]float32{{float32(time.Now().Unix()), float32(v)}}, // TODO FIX ME!
+		Tags:     tags,
+		Hostname: "testhost",
 		// deviceName
 		MetricType: metricType,
 		Interval:   int32(interval),
@@ -59,99 +67,52 @@ func NewPostMetric(name string, v float32, tags string, metricType string, inter
 // This function "starts" the worker by starting a goroutine, that is
 // an infinite "for-select" loop.
 func (w Worker) Start() {
-	// TODO These simple maps won't work when tags are included, as we'll need to key by metric?
-	counters := make(map[Metric]int32)
-	gauges := make(map[Metric]int32)
-	histograms := make(map[Metric]metrics.Histogram)
-
-	mu := &sync.Mutex{}
-
-	ticker := time.NewTicker(time.Duration(w.flushInterval) * time.Second)
-	go func() {
-		for t := range ticker.C {
-			var postMetrics []PostMetric
-			mu.Lock()
-			t.Day()
-			for k, v := range counters {
-				p := NewPostMetric(k.Name, float32(v)/float32(w.flushInterval), k.Tags, "rate", w.flushInterval)
-				postMetrics = append(postMetrics, p)
-				delete(counters, k)
-			}
-			for k, v := range gauges {
-				p := NewPostMetric(k.Name, float32(v), k.Tags, "gauge", w.flushInterval)
-				postMetrics = append(postMetrics, p)
-				delete(gauges, k)
-			}
-			for k, v := range histograms {
-				postMetrics = append(
-					postMetrics,
-					// TODO Is this the right type?
-					NewPostMetric(fmt.Sprintf("%s.count", k.Name), float32(v.Count()), k.Tags, "counter", w.flushInterval),
-				)
-				postMetrics = append(
-					postMetrics,
-					NewPostMetric(fmt.Sprintf("%s.max", k.Name), float32(v.Max()), k.Tags, "gauge", w.flushInterval),
-				)
-				postMetrics = append(
-					postMetrics,
-					NewPostMetric(fmt.Sprintf("%s.min", k.Name), float32(v.Min()), k.Tags, "gauge", w.flushInterval),
-				)
-
-				percentiles := v.Percentiles(w.percentiles)
-				for i, p := range percentiles {
-					postMetrics = append(
-						postMetrics,
-						// TODO Fix to allow for p999, etc
-						NewPostMetric(fmt.Sprintf("%s.%dpercentile", k.Name, int(w.percentiles[i]*100)), float32(p), k.Tags, "gauge", w.flushInterval),
-					)
-				}
-			}
-			mu.Unlock()
-			metricCount := len(postMetrics)
-			if metricCount > 0 {
-				// Make a metric for how many metrics we metriced (be sure to add one to the total for it!)
-				postMetrics = append(
-					postMetrics,
-					// TODO Is this the right type?
-					NewPostMetric("veneur.stats.metrics_posted", float32(metricCount+1), "", "counter", w.flushInterval),
-				)
-				postJSON, _ := json.MarshalIndent(postMetrics, "", "  ")
-				log.Println(string(postJSON))
-			}
-		}
-	}()
 
 	go func() {
 		for {
 			select {
 			case m := <-w.WorkChan:
 				start := time.Now()
-				mu.Lock()
+				w.mutex.Lock()
 				switch m.Type {
 				case "c":
 					// log.Printf("Got counter %q", m.Name)
-					counters[m] += m.Value
+					w.counters[m] += m.Value
 				case "g":
-					gauges[m] = m.Value
+					w.gauges[m] = m.Value
 				case "h", "ms":
-					hist := histograms[m]
+					hist := w.histograms[m]
 					if hist == nil {
 						hist = metrics.NewHistogram(metrics.NewExpDecaySample(1028, 0.015))
-						histograms[m] = hist
+						w.histograms[m] = hist
 					}
 					hist.Update(int64(m.Value))
 				}
-				counters[Metric{Name: "veneur.stats.packets"}]++
-
-				hist := histograms[Metric{Name: "veneur.stats.process_duration_ns"}]
+				// Keep track of how many packets we've processed
+				w.counters[Metric{Name: "veneur.stats.packets", Tags: fmt.Sprintf("worker_id:%d", w.id)}]++
+				// Keep track of how long it took us to process a packet
+				hist := w.histograms[Metric{Name: "veneur.stats.process_duration_ns"}]
 				if hist == nil {
 					hist = metrics.NewHistogram(metrics.NewExpDecaySample(1028, 0.015))
-					ph := Metric{Name: "veneur.stats.process_duration_ns"}
-					histograms[ph] = hist
+					ph := Metric{Name: "veneur.stats.process_duration_ns", Tags: fmt.Sprintf("worker_id:%d", w.id)}
+					w.histograms[ph] = hist
 				}
 				hist.Update(time.Now().Sub(start).Nanoseconds())
 
-				mu.Unlock()
+				w.mutex.Unlock()
+
+			case <-w.FlushChan:
+				log.Println("Received flush signal")
+				start := time.Now()
+				w.Flush()
+				// TODO Dupe name, add method to make this less verbose
+				hist := w.histograms[Metric{Name: "veneur.stats.flush_duration_ns", Tags: fmt.Sprintf("worker_id:%d", w.id)}]
+				if hist == nil {
+					hist = metrics.NewHistogram(metrics.NewExpDecaySample(1028, 0.015))
+					ph := Metric{Name: "veneur.stats.flush_duration_ns"}
+					w.histograms[ph] = hist
+				}
+				hist.Update(time.Now().Sub(start).Nanoseconds())
 
 			case <-w.QuitChan:
 				// We have been asked to stop.
@@ -160,6 +121,47 @@ func (w Worker) Start() {
 			}
 		}
 	}()
+}
+
+func (w Worker) Flush() []PostMetric {
+	var postMetrics []PostMetric
+	w.mutex.Lock()
+	for k, v := range w.counters {
+		p := NewPostMetric(k.Name, float32(v)/float32(w.flushInterval), k.Tags, "rate", w.flushInterval)
+		postMetrics = append(postMetrics, p)
+		delete(w.counters, k)
+	}
+	for k, v := range w.gauges {
+		p := NewPostMetric(k.Name, float32(v), k.Tags, "gauge", w.flushInterval)
+		postMetrics = append(postMetrics, p)
+		delete(w.gauges, k)
+	}
+	for k, v := range w.histograms {
+		postMetrics = append(
+			postMetrics,
+			// TODO Is this the right type?
+			NewPostMetric(fmt.Sprintf("%s.count", k.Name), float32(v.Count()), k.Tags, "counter", w.flushInterval),
+		)
+		postMetrics = append(
+			postMetrics,
+			NewPostMetric(fmt.Sprintf("%s.max", k.Name), float32(v.Max()), k.Tags, "gauge", w.flushInterval),
+		)
+		postMetrics = append(
+			postMetrics,
+			NewPostMetric(fmt.Sprintf("%s.min", k.Name), float32(v.Min()), k.Tags, "gauge", w.flushInterval),
+		)
+
+		percentiles := v.Percentiles(w.percentiles)
+		for i, p := range percentiles {
+			postMetrics = append(
+				postMetrics,
+				// TODO Fix to allow for p999, etc
+				NewPostMetric(fmt.Sprintf("%s.%dpercentile", k.Name, int(w.percentiles[i]*100)), float32(p), k.Tags, "gauge", w.flushInterval),
+			)
+		}
+	}
+	w.mutex.Unlock()
+	return postMetrics
 }
 
 // Stop tells the worker to stop listening for work requests.
