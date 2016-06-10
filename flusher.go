c08ca26b883435b85b0ab77031b4d5d493d98362
Cory Watson <cory@stripe.com>
2016-06-10 08:41:01 -0700

    More timers.

--- flusher.go
+++ flusher.go
@@ -15,6 +15,7 @@ import (
 // Flush takes the slices of metrics, combines then and marshals them to json
 // for posting to Datadog.
 func Flush(postMetrics [][]DDMetric) {
+	pstart := time.Now()
 	totalCount := 0
 	for _, metrics := range postMetrics {
 		totalCount += len(metrics)
@@ -26,34 +27,55 @@ func Flush(postMetrics [][]DDMetric) {
 	for i := range finalMetrics {
 		finalMetrics[i].Hostname = Config.Hostname
 	}
+	Stats.TimeInMilliseconds(
+		"flush.part_duration_ns",
+		float64(time.Now().Sub(pstart).Nanoseconds()),
+		[]string{"part:prepare"},
+		1.0,
+	)
+
 	// Check to see if we have anything to do
 	if totalCount == 0 {
 		log.Info("Nothing to flush, skipping.")
 		return
 	}
 
+	jstart := time.Now()
 	postJSON, err := json.Marshal(map[string][]DDMetric{
 		"series": finalMetrics,
 	})
+	Stats.TimeInMilliseconds(
+		"flush.part_duration_ns",
+		float64(time.Now().Sub(jstart).Nanoseconds()),
+		[]string{"part:json"},
+		1.0,
+	)
 	if err != nil {
 		Stats.Count("flush.error_total", int64(totalCount), []string{"cause:json"}, 1.0)
 		log.WithError(err).Error("Error rendering JSON request body")
 		return
 	}
 
+	cstart := time.Now()
 	var reqBody bytes.Buffer
 	compressor := zlib.NewWriter(&reqBody)
 	// bytes.Buffer never errors
 	compressor.Write(postJSON)
 	// make sure to flush remaining compressed bytes to the buffer
 	compressor.Close()
+	Stats.TimeInMilliseconds(
+		"flush.part_duration_ns",
+		float64(time.Now().Sub(cstart).Nanoseconds()),
+		[]string{"part:compress"},
+		1.0,
+	)
 
 	fstart := time.Now()
 	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/api/v1/series?api_key=%s", Config.APIHostname, Config.Key), &reqBody)
 	Stats.TimeInMilliseconds(
-		"flush.http_duration_ns",
+		"flush.part_duration_ns",
 		float64(time.Now().Sub(fstart).Nanoseconds()),
-		nil,
+		[]string{"part:post"},
 		1.0,
 	)
 	if err != nil {
