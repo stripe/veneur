307a86a056750f09754d55a17079982ec870b7e9
Cory Watson <cory@stripe.com>
2016-09-14 08:37:25 -0700

    Add magic device tag.

--- flusher.go
+++ flusher.go
@@ -174,8 +174,13 @@ func finalizeMetrics(hostname string, tags []string, finalMetrics []DDMetric) []
 			if strings.HasPrefix(tag, "host:") {
 				// delete the tag from the list
 				finalMetrics[i].Tags = append(finalMetrics[i].Tags[:j], finalMetrics[i].Tags[j+1:]...)
-				// Override the hostname with the tag
+				// Override the hostname with the tag, trimming off the prefix
 				finalMetrics[i].Hostname = string(tag[5:len(tag)])
+			} else if strings.HasPrefix(tag, "device:") {
+				// delete the tag from the list
+				finalMetrics[i].Tags = append(finalMetrics[i].Tags[:j], finalMetrics[i].Tags[j+1:]...)
+				// Override the hostname with the tag, trimming off the prefix
+				finalMetrics[i].DeviceName = string(tag[7:len(tag)])
 			}
 		}
 		if finalMetrics[i].Hostname == "" {
--- flusher_test.go
+++ flusher_test.go
@@ -34,3 +34,18 @@ func TestHostMagicTag(t *testing.T) {
 	assert.NotContains(t, metrics[0].Tags, "host:abc123", "Host tag should be removed")
 	assert.Contains(t, metrics[0].Tags, "x:e", "Last tag is still around")
 }
+
+func TestDeviceMagicTag(t *testing.T) {
+	metrics := []DDMetric{{
+		Name:       "foo.bar.baz",
+		Value:      [1][2]float64{{float64(time.Now().Unix()), float64(1.0)}},
+		Tags:       []string{"gorch:frobble", "device:abc123", "x:e"},
+		MetricType: "rate",
+		Interval:   10,
+	}}
+
+	metrics = finalizeMetrics("badhostname", []string{"a:b", "c:d"}, metrics)
+	assert.Equal(t, "abc123", metrics[0].DeviceName, "Metric devicename should be from tag")
+	assert.NotContains(t, metrics[0].Tags, "device:abc123", "Host tag should be removed")
+	assert.Contains(t, metrics[0].Tags, "x:e", "Last tag is still around")
+}
