65b727bde99734e5967e60129b9836c28ea7d13a
Aditya Mukerjee <aditya@stripe.com>
2016-09-19 17:31:31 -0700

    Add documentation to socket.go

--- socket.go
+++ socket.go
@@ -6,6 +6,7 @@ import (
 	"net"
 )
 
+// NewSocket creates a socket which is intended for use by a single goroutine.
 func NewSocket(addr *net.UDPAddr, recvBuf int, reuseport bool) (net.PacketConn, error) {
 	if reuseport {
 		panic("SO_REUSEPORT not supported on this platform")
