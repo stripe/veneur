01eee01b6b9b47c53f2d536feace07d4efcb5bfc
Aditya Mukerjee <dev@chimeracoder.net>
2016-12-19 16:31:35 -0500

    Move SSF protobuf definition file

--- generate.go
+++ generate.go
@@ -1,5 +1,5 @@
 package veneur
 
-//go:generate protoc --go_out=ssf sample.proto
+//go:generate protoc --go_out=. ssf/sample.proto
 //go:generate gojson -input example.yaml -o config.go -fmt yaml -pkg veneur -name Config
 //TODO(aditya) reenable go:generate gojson -input fixtures/datadog_trace.json -o datadog_trace_span.go -fmt json -pkg veneur -name DatadogTraceSpan
