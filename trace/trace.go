b4a8bc1853a82d34e11257617626132581c88b99
Aditya Mukerjee <dev@chimeracoder.net>
2016-12-21 17:03:23 -0500

    Add test for OpenTrace API child span

--- trace/trace.go
+++ trace/trace.go
@@ -8,16 +8,18 @@ import (
 	"reflect"
 	"time"
 
+	"github.com/stripe/veneur/ssf"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/golang/protobuf/proto"
 	"github.com/opentracing/opentracing-go"
 	opentracinglog "github.com/opentracing/opentracing-go/log"
-	"github.com/stripe/veneur/ssf"
 )
 
 var _ opentracing.Tracer = &Tracer{}
 var _ opentracing.Span = &Span{}
 var _ opentracing.SpanContext = &spanContext{}
+var _ opentracing.StartSpanOption = &spanOption{}
 
 type spanContext struct {
 	TraceId      int64
@@ -86,6 +88,8 @@ func (s *Span) SetTag(key string, value interface{}) opentracing.Span {
 	tag := ssf.SSFTag{Name: key}
 	// TODO mutex
 	switch v := value.(type) {
+	case string:
+		tag.Value = v
 	case fmt.Stringer:
 		tag.Value = v.String()
 	default:
@@ -141,13 +145,56 @@ func (s *Span) Log(data opentracing.LogData) {
 type Tracer struct {
 }
 
+type spanOption struct {
+	apply func(*opentracing.StartSpanOptions)
+}
+
+func (so *spanOption) Apply(sso *opentracing.StartSpanOptions) {
+	so.apply(sso)
+}
+
+// customSpanStart returns a StartSpanOption that can be passed to
+// StartSpan, and which will set the created Span's StartTime to the specified
+// value.
+func customSpanStart(t time.Time) opentracing.StartSpanOption {
+	return &spanOption{
+		apply: func(sso *opentracing.StartSpanOptions) {
+			sso.StartTime = t
+		},
+	}
+}
+
+func customSpanTags(k, v string) opentracing.StartSpanOption {
+	return &spanOption{
+		apply: func(sso *opentracing.StartSpanOptions) {
+			if sso.Tags == nil {
+				sso.Tags = map[string]interface{}{}
+			}
+			sso.Tags[k] = v
+		},
+	}
+}
+
+func customSpanParent(t *Trace) opentracing.StartSpanOption {
+	return &spanOption{
+		apply: func(sso *opentracing.StartSpanOptions) {
+			sso.References = append(sso.References, opentracing.SpanReference{
+				Type:              opentracing.ChildOfRef,
+				ReferencedContext: t.context(),
+			})
+		},
+	}
+}
+
 // StartSpan starts a span with the specified operationName (resource) and options.
 // If the options specify a parent span and/or root trace, the resource from the
 // root trace will be used.
 func (t Tracer) StartSpan(operationName string, opts ...opentracing.StartSpanOption) opentracing.Span {
 	// TODO implement References
 
-	sso := opentracing.StartSpanOptions{}
+	sso := opentracing.StartSpanOptions{
+		Tags: map[string]interface{}{},
+	}
 	for _, o := range opts {
 		o.Apply(&sso)
 	}
@@ -193,7 +240,7 @@ func (t Tracer) StartSpan(operationName string, opts ...opentracing.StartSpanOpt
 		}
 
 		span := &Span{
-			Trace:  StartTrace(operationName),
+			Trace:  trace,
 			tracer: t,
 		}
 
@@ -358,6 +405,18 @@ func (t *Trace) SetParent(parent *Trace) {
 	t.ParentId = parent.SpanId
 	t.TraceId = parent.TraceId
 	t.Resource = parent.Resource
+}
+
+// Context returns a spanContext representing the trace
+// from the point of view of its direct children
+func (t *Trace) context() *spanContext {
+	// TODO baggageItems
+	return &spanContext{
+		TraceId:      t.TraceId,
+		Resource:     t.Resource,
+		ParentId:     t.SpanId,
+		baggageItems: nil,
+	}
 }
 
 // StartTrace is called by to create the root-level span
