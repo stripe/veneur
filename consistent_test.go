df49f6be91af8a54f74fa168158761874c54c0b9
Cory Watson <cory@stripe.com>
2017-01-25 15:05:04 -0800

    Fix test

--- consistent_test.go
+++ consistent_test.go
@@ -2,10 +2,12 @@ package veneur
 
 import (
 	"compress/zlib"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -14,9 +16,10 @@ import (
 )
 
 type ConsulTwoRoundTripper struct {
-	t                 *testing.T
-	TwelveGotMetric   bool
-	ThirteenGotMetric bool
+	t         *testing.T
+	wg        *sync.WaitGroup
+	aReceived bool
+	bReceived bool
 }
 
 func (rt *ConsulTwoRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
@@ -31,25 +34,26 @@ func (rt *ConsulTwoRoundTripper) RoundTrip(req *http.Request) (*http.Response, e
 	} else if req.URL.Path == "/import" && req.Host == "10.1.10.12:8000" {
 		z, _ := zlib.NewReader(req.Body)
 		body, _ := ioutil.ReadAll(z)
-		// fmt.Println("12 !!!")
-		// fmt.Println(string(body))
 		defer req.Body.Close()
 		if strings.Contains(string(body), "a.b.c") {
-			rt.TwelveGotMetric = true
+			rt.aReceived = true
 		}
 		rec.Code = http.StatusOK
 	} else if req.URL.Path == "/import" && req.Host == "10.1.10.13:8000" {
 		z, _ := zlib.NewReader(req.Body)
 		body, _ := ioutil.ReadAll(z)
-		// fmt.Println("13")
-		// fmt.Println(string(body))
 		defer req.Body.Close()
 		if strings.Contains(string(body), "x.b.c") {
-			rt.ThirteenGotMetric = true
+			rt.bReceived = true
 		}
 		rec.Code = http.StatusOK
 	} else {
 		assert.Fail(rt.t, "Received an unexpected request: %s %s", req.Host, req.URL.Path)
+	}
+
+	// If we've gotten all of them, fire!
+	if rt.aReceived && rt.bReceived {
+		rt.wg.Done()
 	}
 
 	return rec.Result(), nil
@@ -62,8 +66,11 @@ func TestConsistentForward(t *testing.T) {
 	config.ConsulRefreshInterval = "86400s" // A long time, so we can control it
 	config.Interval = "86400s"
 	config.Debug = true
+	wg := sync.WaitGroup{}
+	wg.Add(1)
 	transport := &ConsulTwoRoundTripper{
-		t: t,
+		t:  t,
+		wg: &wg,
 	}
 	server := setupVeneurServer(t, config, transport)
 
@@ -95,6 +102,17 @@ func TestConsistentForward(t *testing.T) {
 	})
 
 	server.Flush(10*time.Second, 100)
-	assert.True(t, transport.TwelveGotMetric, "Host twelve got metrics!")
-	assert.True(t, transport.ThirteenGotMetric, "Host thirteen got metrics!")
+
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		wg.Wait()
+	}()
+
+	select {
+	case <-c:
+		fmt.Println("GOT 'IM")
+	case <-time.After(3 * time.Second):
+		assert.Fail(t, "Failed to receive all metrics before timeout")
+	}
 }
