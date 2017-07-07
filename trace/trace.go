c7fc039a2cc7844fcedaf0ee4b0ee0997c8e9f78
Aditya Mukerjee <dev@chimeracoder.net>
2017-07-07 10:57:07 -0700

    Dial UDP connection for submitting traces exactly once

--- trace/trace.go
+++ trace/trace.go
@@ -8,6 +8,7 @@ package trace
 
 import (
 	"context"
+	"errors"
 	"io"
 	"math/rand"
 	"net"
@@ -42,6 +43,13 @@ func init() {
 var disabled bool = false
 
 var enabledMtx sync.RWMutex
+
+var udpConn *net.UDPConn
+
+// used to initialize udpConn inside sendSample
+var udpInitOnce sync.Once
+var udpInitFunc = func() { initUDPConn(localVeneurAddress) }
+var udpConnUninitializedErr = errors.New("UDP connection is not yet initialized")
 
 // Make an unexported `key` type that we use as a String such
 // that we don't get lint warnings from using it as a key in
@@ -329,29 +337,38 @@ func sendSample(sample *ssf.SSFSpan) error {
 		return nil
 	}
 
-	serverAddr, err := net.ResolveUDPAddr("udp", localVeneurAddress)
-	if err != nil {
-		return err
+	udpInitOnce.Do(udpInitFunc)
+	if udpConn == nil {
+		return udpConnUninitializedErr
 	}
-
-	conn, err := net.DialUDP("udp", nil, serverAddr)
-	if err != nil {
-		return err
-	}
-
-	defer conn.Close()
 
 	data, err := proto.Marshal(sample)
 	if err != nil {
 		return err
 	}
 
-	_, err = conn.Write(data)
+	_, err = udpConn.Write(data)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+// initUDPConn will initialize the global UDP connection.
+// It will panic if it encounters an error.
+// It should only be called via the corresponding sync.Once
+func initUDPConn(address string) {
+	serverAddr, err := net.ResolveUDPAddr("udp", localVeneurAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := net.DialUDP("udp", nil, serverAddr)
+	if err != nil {
+		panic(err)
+	}
+	udpConn = conn
 }
 
 // stripPackageName strips the package name from a function
