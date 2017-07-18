4ad1ac5597ff2ae6942f529f1024bdae9161c816
Cory Watson <cory@stripe.com>
2017-07-18 09:09:36 -0700

    Flush the LS tracer, since Close() doesn't seem to.

--- flusher.go
+++ flusher.go
@@ -784,6 +784,7 @@ func flushSpansLightstep(ctx context.Context, s *Server, tracerThunk func() open
 	for _, ssfSpan := range ssfSpans {
 		flushSpanLightstep(lightstepTracer, ssfSpan)
 	}
+	lightstep.FlushLightStepTracer(lightstepTracer)
 
 	// Confusingly, this will still get called even if the Opentracing client fails to reach the collector
 	// because we don't get access to the error if that happens.
