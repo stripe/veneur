aa0b2690f786f6c9c37c3ea712031c6d9fe3a145
Cory Watson <cory@stripe.com>
2017-08-11 14:08:57 -0700

    Add first cut of prometheus poller.

---
+++ cmd/veneur-prometheus/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/DataDog/datadog-go/statsd"
+	dto "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
+)
+
+func main() {
+	flag.Parse()
+
+	c, _ := statsd.New("127.0.0.1:8200")
+
+	resp, _ := http.Get("http://localhost:9090/metrics")
+	d := expfmt.NewDecoder(resp.Body, expfmt.FmtText)
+	var mf dto.MetricFamily
+	for {
+		err := d.Decode(&mf)
+		if err != nil {
+			break
+		}
+
+		switch mf.GetType() {
+		case dto.MetricType_COUNTER:
+			for _, counter := range mf.GetMetric() {
+				var tags []string
+				labels := counter.GetLabel()
+				for _, pair := range labels {
+					tags = append(tags, fmt.Sprintf("%s:%s", pair.GetName(), pair.GetValue()))
+				}
+				c.Count(mf.GetName(), int64(counter.GetCounter().GetValue()), tags, 1.0)
+			}
+		}
+	}
+}
