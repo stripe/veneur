00adbbae69220f2555f5244cea2a50ec29bbf315
Yasha Mostofi <yasha.mostofi@gmail.com>
2017-08-15 09:26:29 -0700

    Script to create files

---
+++ file_create.go
@@ -0,0 +1,43 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// 	"path/filepath"
+
+// 	"github.com/Sirupsen/logrus"
+// 	"github.com/gogo/protobuf/proto"
+// 	"github.com/stripe/veneur/ssf"
+// )
+
+// func check(err error) {
+// 	if err != nil {
+// 		logrus.WithError(err).Fatal("rip")
+// 	}
+// }
+
+// func main() {
+// 	span := &ssf.SSFSpan{
+// 		TraceId:        1,
+// 		Id:             1,
+// 		StartTimestamp: 1,
+// 		EndTimestamp:   10,
+// 		Error:          false,
+// 		Service:        "testService",
+// 		Operation:      "operationTest",
+// 		Tags:           map[string]string{"tag1": "value1"},
+// 	}
+// 	buf, _ := proto.Marshal(span)
+// 	fmt.Println(buf)
+
+// 	pbFile := filepath.Join("fixtures", "protobuf", "regression.pb")
+// 	fmt.Println(pbFile)
+// 	file, err := os.Create(pbFile)
+// 	defer file.Close()
+// 	check(err)
+
+// 	var n int
+// 	n, err = file.Write(buf)
+// 	check(err)
+// 	fmt.Printf("n: %d buf: %d\n", n, len(buf))
+// }
