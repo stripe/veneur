be7301317afad68c777ce754645d5c3fe9a163a6
Cory Watson <cory@stripe.com>
2017-09-07 07:26:00 -0700

    Test fixes

--- flusher_test.go
+++ flusher_test.go
@@ -17,47 +17,62 @@ import (
 )
 
 func TestServerTags(t *testing.T) {
-	metrics := []samplers.DDMetric{{
+	ddSink := datadogMetricSink{
+		hostname: "somehostname",
+		tags:     []string{"a:b", "c:d"},
+	}
+
+	metrics := []samplers.InterMetric{{
 		Name:       "foo.bar.baz",
-		Value:      [1][2]float64{{float64(time.Now().Unix()), float64(1.0)}},
+		Timestamp:  time.Now().Unix(),
+		Value:      float64(10),
 		Tags:       []string{"gorch:frobble", "x:e"},
-		MetricType: "rate",
-		Interval:   10,
+		MetricType: "counter",
 	}}
 
-	finalizeMetrics("somehostname", []string{"a:b", "c:d"}, metrics)
-	assert.Equal(t, "somehostname", metrics[0].Hostname, "Metric hostname uses argument")
+	ddMetrics := ddSink.finalizeMetrics(metrics)
+	assert.Equal(t, "somehostname", ddMetrics[0].Hostname, "Metric hostname uses argument")
 	assert.Contains(t, metrics[0].Tags, "a:b", "Tags should contain server tags")
 }
 
 func TestHostMagicTag(t *testing.T) {
-	metrics := []samplers.DDMetric{{
+	ddSink := datadogMetricSink{
+		hostname: "badhostname",
+		tags:     []string{"a:b", "c:d"},
+	}
+
+	metrics := []samplers.InterMetric{{
 		Name:       "foo.bar.baz",
-		Value:      [1][2]float64{{float64(time.Now().Unix()), float64(1.0)}},
+		Timestamp:  time.Now().Unix(),
+		Value:      float64(10),
 		Tags:       []string{"gorch:frobble", "host:abc123", "x:e"},
-		MetricType: "rate",
-		Interval:   10,
+		MetricType: "counter",
 	}}
 
-	finalizeMetrics("badhostname", []string{"a:b", "c:d"}, metrics)
-	assert.Equal(t, "abc123", metrics[0].Hostname, "Metric hostname should be from tag")
-	assert.NotContains(t, metrics[0].Tags, "host:abc123", "Host tag should be removed")
-	assert.Contains(t, metrics[0].Tags, "x:e", "Last tag is still around")
+	ddMetrics := ddSink.finalizeMetrics(metrics)
+	assert.Equal(t, "abc123", ddMetrics[0].Hostname, "Metric hostname should be from tag")
+	assert.NotContains(t, ddMetrics[0].Tags, "host:abc123", "Host tag should be removed")
+	assert.Contains(t, ddMetrics[0].Tags, "x:e", "Last tag is still around")
 }
 
 func TestDeviceMagicTag(t *testing.T) {
-	metrics := []samplers.DDMetric{{
+	ddSink := datadogMetricSink{
+		hostname: "badhostname",
+		tags:     []string{"a:b", "c:d"},
+	}
+
+	metrics := []samplers.InterMetric{{
 		Name:       "foo.bar.baz",
-		Value:      [1][2]float64{{float64(time.Now().Unix()), float64(1.0)}},
+		Timestamp:  time.Now().Unix(),
+		Value:      float64(10),
 		Tags:       []string{"gorch:frobble", "device:abc123", "x:e"},
-		MetricType: "rate",
-		Interval:   10,
+		MetricType: "counter",
 	}}
 
-	finalizeMetrics("badhostname", []string{"a:b", "c:d"}, metrics)
-	assert.Equal(t, "abc123", metrics[0].DeviceName, "Metric devicename should be from tag")
-	assert.NotContains(t, metrics[0].Tags, "device:abc123", "Host tag should be removed")
-	assert.Contains(t, metrics[0].Tags, "x:e", "Last tag is still around")
+	ddMetrics := ddSink.finalizeMetrics(metrics)
+	assert.Equal(t, "abc123", ddMetrics[0].DeviceName, "Metric devicename should be from tag")
+	assert.NotContains(t, ddMetrics[0].Tags, "device:abc123", "Host tag should be removed")
+	assert.Contains(t, ddMetrics[0].Tags, "x:e", "Last tag is still around")
 }
 
 func TestFlushTracesDatadog(t *testing.T) {
