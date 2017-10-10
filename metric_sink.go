6a1bc97a2fcd29a1dd8a167cf4697ad41bf8ed0e
Cory Watson <cory@stripe.com>
2017-10-10 10:30:48 -0700

    Fix bad hostname in sinking events

--- metric_sink.go
+++ metric_sink.go
@@ -133,7 +133,7 @@ func (dd *datadogMetricSink) FlushEventsChecks(ctx context.Context, events []sam
 		// this endpoint is not documented to take an array... but it does
 		// another curious constraint of this endpoint is that it does not
 		// support "Content-Encoding: deflate"
-		err := postHelper(context.TODO(), dd.HTTPClient, dd.statsd, fmt.Sprintf("%s/api/v1/check_run?api_key=%s", dd.hostname, dd.apiKey), checks, "flush_checks", false)
+		err := postHelper(context.TODO(), dd.HTTPClient, dd.statsd, fmt.Sprintf("%s/api/v1/check_run?api_key=%s", dd.ddHostname, dd.apiKey), checks, "flush_checks", false)
 		if err == nil {
 			log.WithField("checks", len(checks)).Info("Completed flushing service checks to Datadog")
 		} else {
