06df1e18d75dec6f27f7297a4e093ac0ee4f733a
Cory Watson <cory@stripe.com>
2017-10-11 08:01:14 -0700

    Add test for splitting post payload.

--- server_test.go
+++ server_test.go
@@ -207,7 +207,6 @@ func newFixture(t *testing.T, config Config) *fixture {
 		if err != nil {
 			t.Fatal(err)
 		}
-
 		f.ddmetrics <- ddmetrics
 		w.WriteHeader(http.StatusAccepted)
 	}))
@@ -257,6 +256,37 @@ func TestLocalServerUnaggregatedMetrics(t *testing.T) {
 	ddmetrics := <-f.ddmetrics
 	assert.Equal(t, 6, len(ddmetrics.Series), "incorrect number of elements in the flushed series on the remote server")
 	assertMetrics(t, ddmetrics, expectedMetrics)
+}
+
+// TestFlushMaxSize tests the behavior of
+// the veneur client splitting a post across multiple requests.
+func TestFlushMaxSize(t *testing.T) {
+	metricValues, _ := generateMetrics()
+	config := localConfig()
+	config.FlushMaxPerBody = 2
+	f := newFixture(t, config)
+	defer f.Close()
+
+	for _, value := range metricValues {
+		f.server.Workers[0].ProcessMetric(&samplers.UDPMetric{
+			MetricKey: samplers.MetricKey{
+				Name: "a.b.c",
+				Type: "histogram",
+			},
+			Value:      value,
+			Digest:     12345,
+			SampleRate: 1.0,
+			Scope:      samplers.LocalOnly,
+		})
+	}
+
+	f.server.Flush()
+
+	// Verify we get 3 sets of 2, other tests can verify the internals
+	for i := 1; i <= 3; i++ {
+		ddmetrics := <-f.ddmetrics
+		assert.Equal(t, 2, len(ddmetrics.Series), "incorrect number of elements in the flushed series on the remote server")
+	}
 }
 
 func TestGlobalServerFlush(t *testing.T) {
