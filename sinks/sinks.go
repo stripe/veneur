666a0a9e27139af22a831ad72839e29fe866fb63
Cory Watson <cory@stripe.com>
2017-11-17 06:48:51 -0800

    Some docs

--- sinks/sinks.go
+++ sinks/sinks.go
@@ -27,10 +27,18 @@ type MetricSink interface {
 // SpanSink is a receiver of spans that handles sending those spans to some
 // downstream sink. Calls to `Ingest(span)` are meant to give the sink control
 // of the span, with periodic calls to flush as a signal for sinks that don't
-// handle their own flushing in a separate goroutine, etc.
+// handle their own flushing in a separate goroutine, etc. Note that SpanSinks
+// differ from MetricSinks because Veneur does *not* aggregate Spans.
 type SpanSink interface {
+	// Start finishes setting up the sink and starts any
+	// background processing tasks that the sink might have to run
+	// in the background. It's invoked when the server starts.
 	Start(*trace.Client) error
 	Name() string
+	// Flush receives `SSFSpan`s from Veneur **as they arrive**. If the sink wants
+	// to buffer spans it may do so and defer sending until `Flush` is called.
 	Ingest(ssf.SSFSpan) error
+	// Invoked at the same interval as metric flushes, this can be used as a
+	// signal for the sink to write out if it was buffering or something.
 	Flush()
 }
