b5fd4cd2edd4801182a7eea75bbcea725d877f32
Aditya Mukerjee <dev@chimeracoder.net>
2017-11-25 14:40:32 -0500

    Range over all containers

--- kubernetes.go
+++ kubernetes.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -36,31 +38,45 @@ func (kd *KubernetesDiscoverer) GetDestinationsForService(serviceName string) ([
 	}
 
 	ips := make([]string, 0, len(pods.Items))
-	for _, pod := range pods.Items {
+	for podIndex, pod := range pods.Items {
 
 		var forwardPort string
 
 		// TODO don't assume there is only one container for the veneur global
-		log.Debug("Found pod %#v", pod)
-		log.Debug("Containers are %#v", pod.Spec.Containers)
+		log.WithField("index", podIndex).Debugf("Found pod %#v", pod)
+		log.WithField("index", podIndex).Debugf("Containers are %#v", pod.Spec.Containers)
 		if len(pod.Spec.Containers) > 0 {
-			for _, port := range pod.Spec.Containers[0].Ports {
-				if port.Name == "http" {
-					forwardPort = strconv.Itoa(int(port.HostPort))
-					log.WithField("port", forwardPort).Debug("Found http port")
-					break
-				}
+			var i int
+			var container v1.Container
+			for i, container = range pod.Spec.Containers {
+				log.WithField("index", container).Debugf("Container %d is %#v", i, container)
+				log.WithField("index", container).Debugf("Container ports are %#v", container.Ports)
+				for _, port := range container.Ports {
+					if port.Name == "http" {
+						forwardPort = strconv.Itoa(int(port.HostPort))
+						log.WithFields(logrus.Fields{
+							"forwardPort": forwardPort,
+							"hostPort":    port.HostPort,
+						}).Debug("Found http port")
+						break
+					}
 
-				// TODO don't assume all TCP ports are for importing
-				if port.Protocol == "TCP" {
-					forwardPort = strconv.Itoa(int(port.HostPort))
-					log.WithField("port", forwardPort).Debug("Found TCP port")
+					// TODO don't assume all TCP ports are for importing
+					if port.Protocol == "TCP" {
+						forwardPort = strconv.Itoa(int(port.HostPort))
+						log.WithField("port", forwardPort).Debug("Found TCP port")
+					}
 				}
 			}
 		}
 
 		if forwardPort == "" {
-			log.Error("Could not find valid port for forwarding")
+			log.WithFields(logrus.Fields{
+				"podIndex":    podIndex,
+				"PodIP":       pod.Status.PodIP,
+				"forwardPort": forwardPort,
+			}).Error("Could not find valid port for forwarding")
+			continue
 		}
 
 		podIp := fmt.Sprintf("%s:%s", pod.Status.PodIP, forwardPort)
