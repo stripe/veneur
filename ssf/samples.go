211523034d9b2b0e9c392cb4ec4487c734f84b2c
Andreas Fuchs <asf@stripe.com>
2017-12-14 13:29:27 +0100

    Add helper functions to construct SSF samples

---
+++ ssf/samples.go
@@ -0,0 +1,37 @@
+package ssf
+
+// Count returns an SSFSample representing an increment / decrement of
+// a counter. It's a convenience wrapper around constructing SSFSample
+// objects.
+func Count(name string, value float32, tags map[string]string) *SSFSample {
+	return *SSFSample{
+		Metric:     SSFSample_COUNTER,
+		Value:      value,
+		Tags:       tags,
+		SampleRate: 1.0,
+	}
+}
+
+// Gauge returns an SSFSample representing a gauge at a certain
+// value. It's a convenience wrapper around constructing SSFSample
+// objects.
+func Gauge(name string, value float32, tags map[string]string) *SSFSample {
+	return &SSFSample{
+		Metric:     SSFSample_GAUGE,
+		Value:      value,
+		Tags:       tags,
+		SampleRate: 1.0,
+	}
+}
+
+// Histogram returns an SSFSample representing a value on a histogram,
+// like a timer or other range. It's a convenience wrapper around
+// constructing SSFSample objects.
+func Histogram(name string, value float32, tags map[string]string) *SSFSample {
+	return &SSFSample{
+		Metric:     SSFSample_HISTOGRAM,
+		Value:      value,
+		Tags:       tags,
+		SampleRate: 1.0,
+	}
+}
