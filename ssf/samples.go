c4f2ef1edf301ae1ef96cc31f7da04b63b80a1b8
Andreas Fuchs <asf@stripe.com>
2017-12-15 17:44:59 +0100

    Introduce functional options for creating SSF samples
    These are only for the stuff you don't need regularly: Units,
    Timestamps.

--- ssf/samples.go
+++ ssf/samples.go
@@ -1,40 +1,69 @@
 package ssf
 
+import "time"
+
+// SampleOption is a functional option that can be used for less
+// commonly needed fields in sample creation helper functions.
+type SampleOption func(*SSFSample)
+
+// Unit is a functional option for creating an SSFSample. It sets the
+// sample's unit name to the name passed.
+func Unit(name string) SampleOption {
+	return func(s *SSFSample) {
+		s.Unit = name
+	}
+}
+
+// Timestamp is a functional option for creating an SSFSample. It sets
+// the timestamp field on the sample to the timestamp passed.
+func Timestamp(ts time.Time) SampleOption {
+	return func(s *SSFSample) {
+		s.Timestamp = ts.UnixNano()
+	}
+}
+
+func create(base *SSFSample, opts []SampleOption) *SSFSample {
+	for _, opt := range opts {
+		opt(base)
+	}
+	return base
+}
+
 // Count returns an SSFSample representing an increment / decrement of
 // a counter. It's a convenience wrapper around constructing SSFSample
 // objects.
-func Count(name string, value float32, tags map[string]string) *SSFSample {
-	return &SSFSample{
+func Count(name string, value float32, tags map[string]string, opts ...SampleOption) *SSFSample {
+	return create(&SSFSample{
 		Metric:     SSFSample_COUNTER,
 		Name:       name,
 		Value:      value,
 		Tags:       tags,
 		SampleRate: 1.0,
-	}
+	}, opts)
 }
 
 // Gauge returns an SSFSample representing a gauge at a certain
 // value. It's a convenience wrapper around constructing SSFSample
 // objects.
-func Gauge(name string, value float32, tags map[string]string) *SSFSample {
-	return &SSFSample{
+func Gauge(name string, value float32, tags map[string]string, opts ...SampleOption) *SSFSample {
+	return create(&SSFSample{
 		Metric:     SSFSample_GAUGE,
 		Name:       name,
 		Value:      value,
 		Tags:       tags,
 		SampleRate: 1.0,
-	}
+	}, opts)
 }
 
 // Histogram returns an SSFSample representing a value on a histogram,
 // like a timer or other range. It's a convenience wrapper around
 // constructing SSFSample objects.
-func Histogram(name string, value float32, tags map[string]string) *SSFSample {
-	return &SSFSample{
+func Histogram(name string, value float32, tags map[string]string, opts ...SampleOption) *SSFSample {
+	return create(&SSFSample{
 		Metric:     SSFSample_HISTOGRAM,
 		Name:       name,
 		Value:      value,
 		Tags:       tags,
 		SampleRate: 1.0,
-	}
+	}, opts)
 }
--- ssf/samples_test.go
+++ ssf/samples_test.go
@@ -7,18 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type constructor func(name string, value float32, tags map[string]string) *SSFSample
+type constructor func(name string, value float32, tags map[string]string, opts ...SampleOption) *SSFSample
 
 func TestValidity(t *testing.T) {
-	tests := []constructor{Count, Gauge, Histogram}
-	for _, elt := range tests {
+	tests := map[string]constructor{"count": Count, "gauge": Gauge, "histogram": Histogram}
+	for name, elt := range tests {
 		test := elt
-		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%s", name), func(t *testing.T) {
 			t.Parallel()
 			sample := test("foo", 1, map[string]string{"purpose": "testing"})
 			assert.Equal(t, "foo", sample.Name)
 			assert.Equal(t, float32(1), sample.Value)
 			assert.Equal(t, map[string]string{"purpose": "testing"}, sample.Tags)
+		})
+		t.Run(fmt.Sprintf("%s/unit", name), func(t *testing.T) {
+			t.Parallel()
+			sample := test("foo", 1, map[string]string{"purpose": "testing"}, Unit("farts"))
+			assert.Equal(t, "foo", sample.Name)
+			assert.Equal(t, float32(1), sample.Value)
+			assert.Equal(t, map[string]string{"purpose": "testing"}, sample.Tags)
+			assert.Equal(t, "farts", sample.Unit)
 		})
 	}
 }
