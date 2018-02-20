888681e5360f09c35899a49320f814657056d0ae
sam boyer <sdboyer@stripe.com>
2018-02-20 13:22:36 -0500

    gofmt over vendor

--- vendor/github.com/googleapis/gnostic/apps/disco/openapiv3.go
+++ vendor/github.com/googleapis/gnostic/apps/disco/openapiv3.go
@@ -18,8 +18,8 @@ import (
 	"net/url"
 	"strings"
 
-	discovery "github.com/googleapis/gnostic/discovery"
 	openapi3 "github.com/googleapis/gnostic/OpenAPIv3"
+	discovery "github.com/googleapis/gnostic/discovery"
 )
 
 func pathForMethod(path string) string {
