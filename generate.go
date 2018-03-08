d0bf90225dbfa84dde6215365c55db37a102275e
Noah Goldman <noahg34@gmail.com>
2018-03-07 17:25:48 -0800

    Update generate.go for the new protobufs
    I added lines to "generate.go" to create the new protobufs that I added.
    I'm not sure if the options I used will work across platforms
    unfortunately, but as far as I can tell I don't think they'll be too
    brittle.  The only dependency I can see is on the google protobuf types,
    but those should typically already be pulled by anyone developing on
    this.

--- generate.go
+++ generate.go
@@ -2,6 +2,9 @@ package veneur
 
 //go:generate protoc --gofast_out=Mssf/sample.proto=github.com/stripe/veneur/ssf,plugins=grpc:. sinks/grpsink/grpc_sink.proto
 //go:generate protoc --gofast_out=. ssf/sample.proto
+//go:generate protoc -I=. -I=$GOPATH/src --gogofaster_out=. tdigest/tdigest.proto
+//go:generate protoc -I=. -I=$GOPATH/src --gogofaster_out=Mtdigest/tdigest.proto=github.com/stripe/veneur/tdigest:. samplers/metricpb/metric.proto
+//go:generate protoc -I=. -I=$GOPATH/src --gogofaster_out=Mtdigest/tdigest.proto=github.com/stripe/veneur/tdigest,Msamplers/metricpb/metric.proto=github.com/stripe/veneur/samplers/metricpb,plugins=grpc:. forwardrpc/forward.proto
 //go:generate gojson -input example.yaml -o config.go -fmt yaml -pkg veneur -name Config
 //go:generate gojson -input example_proxy.yaml -o config_proxy.go -fmt yaml -pkg veneur -name ProxyConfig
 //go:generate stringer -type MetricType samplers
