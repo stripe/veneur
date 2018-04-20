3da276fd89a065c958ae41f3f86d708d427c22f5
sam boyer <sdboyer@stripe.com>
2018-04-19 21:59:01 -0400

    Revert "Reduce math/rand lock contention (#455)"
    This reverts commit 9a00c7a36ab6dd035583007240808bbc61ffe53d.

--- server.go
+++ server.go
@@ -12,7 +12,6 @@ import (
 	"io"
 	"net"
 	"net/http"
-	"runtime"
 	"strings"
 	"sync"
 	"syscall"
@@ -187,19 +186,6 @@ func NewFromConfig(logger *logrus.Logger, conf Config) (*Server, error) {
 
 	if conf.Debug {
 		logger.SetLevel(logrus.DebugLevel)
-	}
-
-	if conf.MutexProfileFraction > 0 {
-		mpf := runtime.SetMutexProfileFraction(conf.MutexProfileFraction)
-		log.WithFields(logrus.Fields{
-			"MutexProfileFraction":         conf.MutexProfileFraction,
-			"previousMutexProfileFraction": mpf,
-		}).Info("Set mutex profile fraction")
-	}
-
-	if conf.BlockProfileRate > 0 {
-		runtime.SetBlockProfileRate(conf.BlockProfileRate)
-		log.WithField("BlockProfileRate", conf.BlockProfileRate).Info("Set block profile rate (nanoseconds)")
 	}
 
 	if conf.EnableProfiling {
@@ -665,28 +651,14 @@ func (s *Server) HandleTracePacket(packet []byte) {
 		log.WithError(err).Warn("ParseSSF")
 		return
 	}
-	// we want to keep track of this, because it's a client problem, but still
-	// handle the span normally
-	if span.Id == 0 {
-		reason := "reason:" + "empty_id"
-		s.Statsd.Count("ssf.error_total", 1, []string{"ssf_format:packet", "packet_type:ssf_metric", reason}, 1.0)
-		log.WithError(err).Warn("ParseSSF")
-	}
-
-	tags := make([]string, 0, 2)
-	s.handleSSF(span, append(tags, "ssf_format:packet"))
+	s.handleSSF(span, []string{"ssf_format:packet"})
 }
 
 func (s *Server) handleSSF(span *ssf.SSFSpan, baseTags []string) {
-	// 1/sampleRate packets will be chosen
-	const sampleRate = 10
-
 	tags := append(baseTags, "service:"+span.Service)
 
-	if (span.Id % sampleRate) == 1 {
-		s.Statsd.Incr("ssf.spans.received_total", tags, 1)
-		s.Statsd.Histogram("ssf.spans.tags_per_span", float64(len(span.Tags)), tags, 1)
-	}
+	s.Statsd.Incr("ssf.spans.received_total", tags, .1)
+	s.Statsd.Histogram("ssf.spans.tags_per_span", float64(len(span.Tags)), tags, .1)
 
 	s.SpanChan <- span
 }
@@ -763,37 +735,37 @@ func (s *Server) ReadSSFStreamSocket(serverConn net.Conn) {
 	defer func() {
 		serverConn.Close()
 	}()
-
-	// initialize the capacity to the max size
-	// based on the number of tags we add later
-	tags := make([]string, 1, 3)
-	tags[0] = "ssf_format:framed"
-
+	tags := map[string]string{"ssf_format": "framed"}
 	for {
 		msg, err := protocol.ReadSSF(serverConn)
 		if err != nil {
 			if err == io.EOF {
 				// Client hangup, close this
-				s.Statsd.Count("frames.disconnects", 1, nil, 1.0)
+				metrics.ReportOne(s.TraceClient, ssf.Count("frames.disconnects", 1, nil))
 				return
 			}
 			if protocol.IsFramingError(err) {
 				log.WithError(err).
 					WithField("remote", serverConn.RemoteAddr()).
 					Info("Frame error reading from SSF connection. Closing.")
-				tags = append(tags, []string{"packet_type:unknown", "reason:framing"}...)
-				s.Statsd.Incr("ssf.error_total", tags, 1.0)
+				sample := ssf.Count("ssf.error_total", 1, tags)
+				sample.Tags["packet_type"] = "unknown"
+				sample.Tags["reason"] = "framing"
+				metrics.ReportOne(s.TraceClient, sample)
 				return
 			}
 			// Non-frame errors means we can continue reading:
 			log.WithError(err).
 				WithField("remote", serverConn.RemoteAddr()).
 				Error("Error processing an SSF frame")
-			tags = append(tags, []string{"packet_type:unknown", "reason:processing"}...)
-			s.Statsd.Incr("ssf.error_total", tags, 1.0)
+			sample := ssf.Count("ssf.error_total", 1, tags)
+			sample.Tags["packet_type"] = "unknown"
+			sample.Tags["reason"] = "processing"
+			metrics.ReportOne(s.TraceClient, sample)
 			continue
 		}
-		s.Statsd.Incr("ssf.received_total", tags, 1)
+		metrics.ReportBatch(s.TraceClient,
+			ssf.RandomlySample(0.01, ssf.Count("ssf.received_total", 1, tags)))
 		s.handleSSF(msg, []string{"ssf_format:framed"})
 	}
 }
