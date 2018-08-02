7e79137ddb7ef855b7ccb6575627b70789faf9aa
Cory Watson <cory@stripe.com>
2018-08-02 09:30:20 -0700

    More tuning of capacity for client

--- proxy.go
+++ proxy.go
@@ -196,7 +196,7 @@ func NewProxyFromConfig(logger *logrus.Logger, conf ProxyConfig) (p Proxy, err e
 
 		p.TraceClient, err = trace.NewClient(conf.SsfDestinationAddress,
 			trace.Buffered,
-			trace.Capacity(128),
+			trace.Capacity(1024),
 			trace.FlushInterval(3*time.Second),
 			trace.ReportStatistics(stats, 1*time.Second, []string{format}),
 		)
