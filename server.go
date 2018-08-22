84fd0e564650ded9518c93fb533c6a5fced37392
Andreas Fuchs <asf@stripe.com>
2018-08-22 16:31:02 +0200

    Correctly initialize the splunk span sink
    I'd messed up the order of hostnames. Oops!

--- server.go
+++ server.go
@@ -416,7 +416,7 @@ func NewFromConfig(logger *logrus.Logger, conf Config) (*Server, error) {
 			return ret, fmt.Errorf("both splunk_hec_address and splunk_hec_token need to be set!")
 		}
 		if conf.SplunkHecToken != "" && conf.SplunkHecAddress != "" {
-			sss, err := splunk.NewSplunkSpanSink(conf.SplunkHecAddress, conf.SplunkHecToken, conf.SplunkHecTLSValidateHostname, conf.Hostname, log)
+			sss, err := splunk.NewSplunkSpanSink(conf.SplunkHecAddress, conf.SplunkHecToken, conf.Hostname, conf.SplunkHecTLSValidateHostname, log)
 			if err != nil {
 				return ret, err
 			}
