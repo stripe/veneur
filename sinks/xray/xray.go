25fc5b9ee81b60018461984cbc0ad81e1cad3f79
Aditya Mukerjee <dev@chimeracoder.net>
2018-11-02 13:46:13 -0400

    Change log line to debug

--- sinks/xray/xray.go
+++ sinks/xray/xray.go
@@ -216,7 +216,7 @@ func (x *XRaySpanSink) Flush() {
 	x.log.WithFields(logrus.Fields{
 		"flushed_spans": atomic.LoadInt64(&x.spansHandled),
 		"dropped_spans": atomic.LoadInt64(&x.spansDropped),
-	}).Info("Checkpointing flushed spans for X-Ray")
+	}).Debug("Checkpointing flushed spans for X-Ray")
 	metrics.ReportBatch(x.traceClient, []*ssf.SSFSample{
 		ssf.Count(sinks.MetricKeyTotalSpansFlushed, float32(atomic.SwapInt64(&x.spansHandled, 0)), map[string]string{"sink": x.Name()}),
 		ssf.Count(sinks.MetricKeyTotalSpansDropped, float32(atomic.SwapInt64(&x.spansDropped, 0)), map[string]string{"sink": x.Name()}),
