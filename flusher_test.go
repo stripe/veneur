ec15929173ec3b85070242a76d51ea3ed006c38b
Andreas Fuchs <asf-stripe@users.noreply.github.com>
2019-04-03 15:33:08 -0700

    Clear up the Server.Flush method some more (#717)
    * Make Server.Flush synchronous
    
    This helps us clear up the context-cancelation story in Server.Flush,
    and it makes the function more clearly testable. Previously, it
    spawned goroutines that terminated "sometime later" (and hopefully
    before the next flush). Now, we wait for the forward to finish before
    returning from Flush; context cancelation should ensure that veneur
    really doesn't get stuck anymore.
    
    Also, cancel any in-flight flush context as soon as we detect that the
    server is terminating.
    
    * Fix a test timeout around flushing
    
    This test called Flush, expecting the flush to not wait; it does wait
    however, so let's rely on the running server's flush. To ensure the
    test doesn't terminate before flush is done, wait until we receive a
    local metric.
    
    * Make the "unreachable destinations" test abort sooner
    
    This should reduce the number of times this test times out locally a
    bit.

--- flusher_test.go
+++ flusher_test.go
@@ -26,6 +26,7 @@ func TestServerFlushGRPC(t *testing.T) {
 	defer testServer.Stop()
 
 	localCfg := localConfig()
+	localCfg.DebugFlushedMetrics = true
 	localCfg.ForwardAddress = testServer.Addr().String()
 	localCfg.ForwardUseGrpc = true
 	local := setupVeneurServer(t, localCfg, nil, nil, nil)
@@ -35,8 +36,6 @@ func TestServerFlushGRPC(t *testing.T) {
 	for _, input := range inputs {
 		local.Workers[0].ProcessMetric(input)
 	}
-
-	local.Flush(context.Background())
 
 	expected := []string{
 		testGRPCMetric("histogram"),
@@ -48,25 +47,49 @@ func TestServerFlushGRPC(t *testing.T) {
 		testGRPCMetric("set"),
 	}
 
+	// Wait until the running server has flushed out the metrics for us:
 	select {
 	case v := <-done:
+		log.Print("got the goods")
 		assert.ElementsMatch(t, expected, v,
 			"Flush didn't output the right metrics")
 	case <-time.After(time.Second):
+		log.Print("timed out")
 		t.Fatal("Timed out waiting for the gRPC server to receive the flush")
 	}
 }
 
 // Just test that a flushing to a bad address is handled without panicing
 func TestServerFlushGRPCBadAddress(t *testing.T) {
+	rcv := make(chan []samplers.InterMetric, 10)
+	sink, err := NewChannelMetricSink(rcv)
+	require.NoError(t, err)
+
 	localCfg := localConfig()
 	localCfg.ForwardAddress = "bad-address:123"
 	localCfg.ForwardUseGrpc = true
-	local := setupVeneurServer(t, localCfg, nil, nil, nil)
+
+	local := setupVeneurServer(t, localCfg, nil, sink, nil)
 	defer local.Shutdown()
 
 	local.Workers[0].ProcessMetric(forwardGRPCTestMetrics()[0])
-	local.Flush(context.Background())
+	m := samplers.UDPMetric{
+		MetricKey: samplers.MetricKey{
+			Name: "counter",
+			Type: counterTypeName,
+		},
+		Value:      20.0,
+		Digest:     12345,
+		SampleRate: 1.0,
+		Scope:      samplers.MixedScope,
+	}
+	local.Workers[0].ProcessMetric(&m)
+	select {
+	case <-rcv:
+		t.Log("Received my metric, assume this is working.")
+	case <-time.After(time.Second * 5):
+		t.Fatal("timed out waiting for global veneur flush")
+	}
 }
 
 // Ensure that if someone sends a histogram to the global stats box directly,
