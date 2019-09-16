497a540b9a4b9fb6ed6deb6d589f727d1549ffe2
Kasey Klipsch <kklipsch@stripe.com>
2019-09-16 14:13:42 -0500

    Test prometheus code against prometheus

--- cmd/veneur-prometheus/main_test.go
+++ cmd/veneur-prometheus/main_test.go
@@ -3,15 +3,75 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
 	"testing"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	dto "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestPrometheusCounterIsEverIncreasing(t *testing.T) {
+	counter := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "counter",
+		Help: "A typical counter.",
+	})
+
+	err := prometheus.Register(counter)
+	require.NoError(t, err)
+
+	ts := httptest.NewServer(promhttp.Handler())
+	defer ts.Close()
+
+	count, err := getCount(ts.URL, "counter")
+	require.NoError(t, err)
+	assert.Equal(t, 0.0, count)
+
+	counter.Inc()
+
+	count, err = getCount(ts.URL, "counter")
+	require.NoError(t, err)
+	assert.Equal(t, 1.0, count)
+
+	counter.Inc()
+
+	count, err = getCount(ts.URL, "counter")
+	require.NoError(t, err)
+	assert.Equal(t, 2.0, count)
+}
+
+func getCount(url string, name string) (float64, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+	defer res.Body.Close()
+
+	d := expfmt.NewDecoder(res.Body, expfmt.FmtText)
+	var mf dto.MetricFamily
+	for {
+		err := d.Decode(&mf)
+		if err == io.EOF {
+			return 0, fmt.Errorf("did not find count")
+		} else if err != nil {
+			return 0, err
+		}
+
+		if mf.GetName() == name {
+			for _, counter := range mf.GetMetric() {
+				return counter.GetCounter().GetValue(), nil
+			}
+		}
+	}
+}
 
 func TestGetTags(t *testing.T) {
 	label1Name := "label1Name"
