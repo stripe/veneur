c86de78cc159511d8e2ba732c01e327fd8e36789
Shrivu Shankar <shrivu@stripe.com>
2020-08-12 09:14:16 -0700

    tweaked struct ordering

--- config.go
+++ config.go
@@ -29,6 +29,7 @@ type Config struct {
 	ForwardAddress                            string    `yaml:"forward_address"`
 	ForwardUseGrpc                            bool      `yaml:"forward_use_grpc"`
 	GrpcAddress                               string    `yaml:"grpc_address"`
+	GrpcListenAddresses                       []string  `yaml:"grpc_listen_addresses"`
 	Hostname                                  string    `yaml:"hostname"`
 	HTTPAddress                               string    `yaml:"http_address"`
 	HTTPQuit                                  bool      `yaml:"http_quit"`
@@ -94,7 +95,6 @@ type Config struct {
 	SplunkSpanSampleRate              int      `yaml:"splunk_span_sample_rate"`
 	SsfBufferSize                     int      `yaml:"ssf_buffer_size"`
 	SsfListenAddresses                []string `yaml:"ssf_listen_addresses"`
-	GrpcListenAddresses               []string `yaml:"grpc_listen_addresses"`
 	StatsAddress                      string   `yaml:"stats_address"`
 	StatsdListenAddresses             []string `yaml:"statsd_listen_addresses"`
 	SynchronizeWithInterval           bool     `yaml:"synchronize_with_interval"`
