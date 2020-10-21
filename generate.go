b6e103a3bdbaa2ada31ff1b084f0d8dfc2fe9a07
Aditya Mukerjee <dev@chimeracoder.net>
2020-10-21 18:19:10 -0400

    Update generate.go

--- generate.go
+++ generate.go
@@ -1,10 +1,10 @@
 package veneur
 
-//go:generate protoc --gogofaster_out=Mssf/sample.proto=github.com/stripe/veneur/ssf,plugins=grpc:. sinks/grpsink/grpc_sink.proto
-//go:generate protoc --gogofaster_out=. ssf/sample.proto
-//go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gogofaster_out=. tdigest/tdigest.proto
-//go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gogofaster_out=Mtdigest/tdigest.proto=github.com/stripe/veneur/tdigest:. samplers/metricpb/metric.proto
-//go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gogofaster_out=Mtdigest/tdigest.proto=github.com/stripe/veneur/tdigest,Msamplers/metricpb/metric.proto=github.com/stripe/veneur/samplers/metricpb,Mgoogle/protobuf/empty.proto=github.com/golang/protobuf/ptypes/empty,plugins=grpc:. forwardrpc/forward.proto
+//go:generate protoc --go_opt=paths=source_relative --go_out=. --go-grpc_out=. --go-grpc_opt=paths=source_relative sinks/grpsink/grpc_sink.proto
+//go:generate protoc --go_opt=paths=source_relative --go_out=. ssf/sample.proto
+//go:generate protoc --go_opt=paths=source_relative -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf@v1.3.0 --gogofaster_out=. tdigest/tdigest.proto
+//go:generate protoc --go_opt=paths=source_relative -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gogofaster_out=Mtdigest/tdigest.proto=github.com/stripe/veneur/tdigest:. samplers/metricpb/metric.proto
+//go:generate protoc --go_opt=paths=source_relative -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gogofaster_out=Mtdigest/tdigest.proto=github.com/stripe/veneur/tdigest,Msamplers/metricpb/metric.proto=github.com/stripe/veneur/samplers/metricpb,Mgoogle/protobuf/empty.proto=github.com/golang/protobuf/ptypes/empty,plugins=grpc:. forwardrpc/forward.proto
 //go:generate gojson -input example.yaml -o config.go -fmt yaml -pkg veneur -name Config
 //go:generate gojson -input example_proxy.yaml -o config_proxy.go -fmt yaml -pkg veneur -name ProxyConfig
 //go:generate stringer -type MetricType samplers
