ae20a859230fe4117e225ac16c754df42b4a6d67
Aditya Mukerjee <dev@chimeracoder.net>
2020-10-22 10:12:14 -0400

    Update protobuf generation

--- generate.go
+++ generate.go
@@ -1,9 +1,10 @@
 package veneur
 
 //go:generate protoc --go_opt=paths=source_relative --go_out=. --go-grpc_out=. --go-grpc_opt=paths=source_relative sinks/grpsink/grpc_sink.proto
+//go:generate protoc --go_opt=paths=source_relative --go_out=. tdigest/gogoproto/gogo.proto
 //go:generate protoc --go_opt=paths=source_relative --go_out=. ssf/sample.proto
-//go:generate protoc --go_opt=paths=source_relative --go_out=. tdigest/tdigest.proto
-//go:generate protoc --go_opt=paths=source_relative --go_out=. samplers/metricpb/metric.proto
+//go:generate protoc --go_opt=paths=source_relative --go_out=. -I=$GOPATH/src -I=. tdigest/tdigest.proto
+//go:generate protoc --go_opt=paths=source_relative --go_out=. -I=$GOPATH/src -I=. samplers/metricpb/metric.proto
 //go:generate protoc --go_opt=paths=source_relative --go_out=. forwardrpc/forward.proto
 //go:generate gojson -input example.yaml -o config.go -fmt yaml -pkg veneur -name Config
 //go:generate gojson -input example_proxy.yaml -o config_proxy.go -fmt yaml -pkg veneur -name ProxyConfig
