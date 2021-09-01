5ffd762d7155c07971fb859b1ae1ab6d744040b6
Philip Norman <philip@stripe.com>
2021-09-01 13:02:26 -0700

    Create mock server for Cortex sink tests
    Co-authored-by: Philip Norman <philipnrmn@users.noreply.github.com>

--- sinks/cortex/cortex_test.go
+++ sinks/cortex/cortex_test.go
@@ -1,8 +1,16 @@
 package cortex
 
 import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/snappy"
+	"github.com/prometheus/prometheus/prompb"
 	"github.com/stretchr/testify/assert"
 	"github.com/stripe/veneur/v14/sinks"
 )
@@ -14,4 +22,75 @@ func TestName(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, "cortex", sink.Name())
+}
+
+func TestFlush(t *testing.T) {
+	server := NewTestServer()
+	defer server.Close()
+	// TODO implement flush test
+}
+
+// TestServer wraps an internal httptest.Server and provides a convenience
+// method for retrieving the most recently written series
+type TestServer struct {
+	URL    string
+	data   *prompb.WriteRequest
+	server *httptest.Server
+}
+
+// Close closes the internal test server
+func (t *TestServer) Close() {
+	t.server.Close()
+}
+
+// Latest returns the most recent write request, or errors if there was none
+func (t *TestServer) Latest() (*prompb.WriteRequest, error) {
+	if t.data == nil {
+		return nil, errors.New("no data received")
+	}
+	return t.data, nil
+}
+
+// NewTestServer starts a test server instance. Ensure calls are followed by
+// defer server.Close()
+// to avoid hanging connections
+func NewTestServer() *TestServer {
+	result := TestServer{}
+
+	router := http.NewServeMux()
+	router.HandleFunc("/receive", func(w http.ResponseWriter, r *http.Request) {
+		wr, err := readpb(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		// keep a record of the most recently received request
+		result.data = wr
+	})
+
+	server := httptest.NewServer(router)
+	result.URL = server.URL
+	result.server = server
+
+	return &result
+}
+
+// readpb reads, decompresses and unmarshals a WriteRequest from a reader
+func readpb(r io.Reader) (*prompb.WriteRequest, error) {
+	cdata, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := snappy.Decode(nil, cdata)
+	if err != nil {
+		return nil, err
+	}
+
+	var wr prompb.WriteRequest
+	if err := proto.Unmarshal(data, &wr); err != nil {
+		return nil, err
+	}
+
+	return &wr, nil
 }
