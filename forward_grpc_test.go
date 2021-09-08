d8e2f52a646ff48b4d63418cf9ca4a6523d91d2d
Arnav Dugar <87779081+arnavdugar-stripe@users.noreply.github.com>
2021-09-08 12:28:45 -0700

    Fix sync.Mutex copy. (#852)

--- forward_grpc_test.go
+++ forward_grpc_test.go
@@ -47,13 +47,13 @@ func newForwardGRPCFixture(t testing.TB, localConfig Config, sink sinks.MetricSi
 	go func() {
 		proxy.Serve()
 	}()
-	waitForHTTPStart(t, &proxy, 3*time.Second)
+	waitForHTTPStart(t, proxy, 3*time.Second)
 
 	localConfig.ForwardAddress = proxyCfg.GrpcAddress
 	localConfig.ForwardUseGrpc = true
 	local := setupVeneurServer(t, localConfig, nil, nil, nil, nil)
 
-	return &forwardGRPCFixture{t: t, proxy: &proxy, global: global, local: local}
+	return &forwardGRPCFixture{t: t, proxy: proxy, global: global, local: local}
 }
 
 // stop stops all of the various servers inside the fixture.
