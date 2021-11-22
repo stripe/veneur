54141b4b30c35ea2105743c8c1f0f4be5b8884b0
Arnav Dugar <87779081+arnavdugar-stripe@users.noreply.github.com>
2021-11-22 15:02:48 -0800

    Add mock sinks for testing. (#879)

--- generate.go
+++ generate.go
@@ -9,3 +9,4 @@ package veneur
 //go:generate protoc -I=. -I=$GOPATH/pkg/mod -I=$GOPATH/pkg/mod/github.com/gogo/protobuf@v1.2.1/protobuf --gogofaster_out=Mtdigest/tdigest.proto=github.com/stripe/veneur/v14/tdigest,Msamplers/metricpb/metric.proto=github.com/stripe/veneur/v14/samplers/metricpb,Mgoogle/protobuf/empty.proto=github.com/golang/protobuf/ptypes/empty,plugins=grpc:. forwardrpc/forward.proto
 //go:generate stringer -type MetricType ./samplers
 //TODO(aditya) reenable go:generate gojson -input fixtures/datadog_trace.json -o datadog_trace_span.go -fmt json -pkg veneur -name DatadogTraceSpan
+//go:generate mockgen -source=sinks/sinks.go -destination=sinks/mock/mock.go -package=mock
