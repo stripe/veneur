bc0fd630e74303a12abf2eb17c5b1a6c7188c492
rma-stripe <48223048+rma-stripe@users.noreply.github.com>
2022-03-22 11:36:33 -0700

    add strip_tags config option for cloudwatch sink (#918)
    Co-authored-by: Randall Ma <randall@randallma.com>

--- sinks/cloudwatch/cloudwatch_test.go
+++ sinks/cloudwatch/cloudwatch_test.go
@@ -64,7 +64,10 @@ func latest(reqBodyCh chan []byte, timeoutSecs int) ([]byte, error) {
 }
 
 func TestName(t *testing.T) {
-	sink := NewCloudwatchMetricSink("http://localhost/", "test", "us-east-1000", "cloudwatch_standard_unit", time.Second*30, true, logrus.NewEntry(logrus.New()))
+	sink := NewCloudwatchMetricSink(
+		"http://localhost/", "test", "us-east-1000", "cloudwatch_standard_unit",
+		time.Second*30, true, []string{}, logrus.NewEntry(logrus.New()),
+	)
 	assert.Equal(t, "cloudwatch", sink.Name())
 }
 
@@ -81,7 +84,10 @@ func TestFlush(t *testing.T) {
 	assert.NoError(t, json.Unmarshal(jsInput, &metrics))
 
 	// Initialize sink
-	sink := NewCloudwatchMetricSink(server.URL, "test", "us-east-1000", "cloudwatch_standard_unit", time.Second*30, true, logrus.NewEntry(logrus.New()))
+	sink := NewCloudwatchMetricSink(
+		server.URL, "test", "us-east-1000", "cloudwatch_standard_unit",
+		time.Second*30, true, []string{}, logrus.NewEntry(logrus.New()),
+	)
 	sink.Start(nil)
 
 	// Assert data is as we expect
@@ -115,13 +121,56 @@ func TestFlushWithStandardUnitTagName(t *testing.T) {
 	assert.NoError(t, json.Unmarshal(jsInput, &metrics))
 
 	// Initialize sink
-	sink := NewCloudwatchMetricSink(server.URL, "test", "us-east-1000", "cloudwatch_standard_unit", time.Second*30, true, logrus.NewEntry(logrus.New()))
+	sink := NewCloudwatchMetricSink(
+		server.URL, "test", "us-east-1000", "cloudwatch_standard_unit",
+		time.Second*30, true, []string{}, logrus.NewEntry(logrus.New()),
+	)
 	sink.Start(nil)
 
 	// Inspect data that was flushed, which should have a standard unit and no dimensions
 	done := make(chan bool)
 	go func(reqBodyCh chan []byte, done chan bool) {
 		expectedOutput, err := ioutil.ReadFile("testdata/output.2.txt")
+		assert.NoError(t, err)
+		data, err := latest(reqBodyCh, 3)
+		assert.NoError(t, err)
+		assert.Equal(t, string(expectedOutput), string(data))
+		done <- true
+	}(reqBodyCh, done)
+
+	// Flush the sink
+	err = sink.Flush(context.Background(), metrics)
+	assert.NoError(t, err)
+
+	<-done
+}
+
+func TestFlushWithStripTags(t *testing.T) {
+	// Listen for PutMetricData
+	reqBodyCh := make(chan []byte)
+	server := NewTestServer(t, 0, reqBodyCh)
+	defer server.Close()
+
+	// input.2.json contains timeseries with tags to strip, and tags to keep
+	jsInput, err := ioutil.ReadFile("testdata/input.3.json")
+	assert.NoError(t, err)
+	var metrics []samplers.InterMetric
+	assert.NoError(t, json.Unmarshal(jsInput, &metrics))
+
+	// Initialize sink
+	stripTags := []string{"baz"}
+	sink := NewCloudwatchMetricSink(
+		server.URL, "test", "us-east-1000", "cloudwatch_standard_unit",
+		time.Second*30, true, stripTags, logrus.NewEntry(logrus.New()),
+	)
+	sink.Start(nil)
+
+	// Inspect data that was flushed
+	// - Datapoint (1, 3) should not have tags
+	// - Datapoint (2) should have one tag
+	done := make(chan bool)
+	go func(reqBodyCh chan []byte, done chan bool) {
+		expectedOutput, err := ioutil.ReadFile("testdata/output.3.txt")
 		assert.NoError(t, err)
 		data, err := latest(reqBodyCh, 3)
 		assert.NoError(t, err)
@@ -146,7 +195,10 @@ func TestFlushNoop(t *testing.T) {
 	var metrics []samplers.InterMetric
 
 	// Initialize the sink
-	sink := NewCloudwatchMetricSink(server.URL, "test", "us-east-1000", "cloudwatch_standard_unit", time.Second*30, true, logrus.NewEntry(logrus.New()))
+	sink := NewCloudwatchMetricSink(
+		server.URL, "test", "us-east-1000", "cloudwatch_standard_unit",
+		time.Second*30, true, []string{}, logrus.NewEntry(logrus.New()),
+	)
 	sink.Start(nil)
 
 	// Assert the server was never hit
@@ -178,7 +230,10 @@ func TestFlushRemoteTimeout(t *testing.T) {
 	metrics := []samplers.InterMetric{{}}
 
 	// Initialize the sink
-	sink := NewCloudwatchMetricSink(server.URL, "test", "us-east-1000", "cloudwatch_standard_unit", customTimeout, true, logrus.NewEntry(logrus.New()))
+	sink := NewCloudwatchMetricSink(
+		server.URL, "test", "us-east-1000", "cloudwatch_standard_unit",
+		customTimeout, true, []string{}, logrus.NewEntry(logrus.New()),
+	)
 	sink.Start(nil)
 
 	// Make sure that the server can Close, eventually
