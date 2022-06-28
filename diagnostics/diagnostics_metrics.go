17d69af0110c75c0722805cb35d96d099899fef7
Alan Chu <chualan@stripe.com>
2022-06-28 15:01:09 -0700

    Prepend memory metrics name with mem

--- diagnostics/diagnostics_metrics.go
+++ diagnostics/diagnostics_metrics.go
@@ -37,68 +37,68 @@ func CollectUptimeMetrics(statsd scopedstatsd.Client, interval time.Duration, ta
 // This function take inspiration from the prometheus golang code base: https://github.com/prometheus/client_golang/blob/24172847e35ba46025c49d90b8846b59eb5d9ead/prometheus/go_collector.go
 func CollectRuntimeMemStats(statsd scopedstatsd.Client, memstatsCurrent *runtime.MemStats, memstatsPrev *runtime.MemStats, tags []string) {
 	// Collect number of bytes obtained from system.
-	statsd.Gauge("sys_bytes", float64(memstatsCurrent.Sys), tags, 1)
+	statsd.Gauge("mem.sys_bytes", float64(memstatsCurrent.Sys), tags, 1)
 
 	// Collect number of pointer lookups.
-	statsd.Gauge("pointer_lookups", float64(memstatsCurrent.Lookups), tags, 1)
+	statsd.Gauge("mem.pointer_lookups", float64(memstatsCurrent.Lookups), tags, 1)
 
 	// Collect increased heap objects allocated compared to last flush.
-	statsd.Count("mallocs_total", int64(memstatsCurrent.Mallocs-memstatsPrev.Mallocs), tags, 1)
+	statsd.Count("mem.mallocs_total", int64(memstatsCurrent.Mallocs-memstatsPrev.Mallocs), tags, 1)
 
 	// Collect increased heap objects freed compared to last flush.
-	statsd.Count("frees_total", int64(memstatsCurrent.Frees-memstatsPrev.Frees), tags, 1)
+	statsd.Count("mem.frees_total", int64(memstatsCurrent.Frees-memstatsPrev.Frees), tags, 1)
 
 	// Collect number of mallocs.
-	statsd.Gauge("mallocs_count", float64(memstatsCurrent.Mallocs-memstatsCurrent.Frees), tags, 1)
+	statsd.Gauge("mem.mallocs_count", float64(memstatsCurrent.Mallocs-memstatsCurrent.Frees), tags, 1)
 
 	// Collect number of bytes newly allocated for heap objects compared to last flush.
-	statsd.Count("heap_alloc_bytes_total", int64(memstatsCurrent.TotalAlloc-memstatsPrev.TotalAlloc), tags, 1)
+	statsd.Count("mem.heap_alloc_bytes_total", int64(memstatsCurrent.TotalAlloc-memstatsPrev.TotalAlloc), tags, 1)
 
 	// Collect number of heap bytes allocated and still in use.
-	statsd.Gauge("heap_alloc_bytes", float64(memstatsCurrent.HeapAlloc), tags, 1)
+	statsd.Gauge("mem.heap_alloc_bytes", float64(memstatsCurrent.HeapAlloc), tags, 1)
 
 	// Collect number of heap bytes obtained from system.
-	statsd.Gauge("heap_sys_bytes", float64(memstatsCurrent.HeapSys), tags, 1)
+	statsd.Gauge("mem.heap_sys_bytes", float64(memstatsCurrent.HeapSys), tags, 1)
 
 	// Collect number of heap bytes waiting to be used.
-	statsd.Gauge("heap_idle_bytes", float64(memstatsCurrent.HeapIdle), tags, 1)
+	statsd.Gauge("mem.heap_idle_bytes", float64(memstatsCurrent.HeapIdle), tags, 1)
 
 	// Collect number of heap bytes that are in use.
-	statsd.Gauge("heap_inuse_bytes", float64(memstatsCurrent.HeapInuse), tags, 1)
+	statsd.Gauge("mem.heap_inuse_bytes", float64(memstatsCurrent.HeapInuse), tags, 1)
 
 	// Collect number of heap bytes released to OS.
-	statsd.Gauge("heap_released_bytes", float64(memstatsCurrent.HeapReleased), tags, 1)
+	statsd.Gauge("mem.heap_released_bytes", float64(memstatsCurrent.HeapReleased), tags, 1)
 
 	// Collect number of allocated objects.
-	statsd.Gauge("heap_objects_count", float64(memstatsCurrent.HeapObjects), tags, 1)
+	statsd.Gauge("mem.heap_objects_count", float64(memstatsCurrent.HeapObjects), tags, 1)
 
 	// Collect number of bytes in use by the stack allocator.
-	statsd.Gauge("stack_inuse_bytes", float64(memstatsCurrent.StackInuse), tags, 1)
+	statsd.Gauge("mem.stack_inuse_bytes", float64(memstatsCurrent.StackInuse), tags, 1)
 
 	// Collect number of bytes obtained from system for stack allocator.
-	statsd.Gauge("stack_sys_bytes", float64(memstatsCurrent.StackSys), tags, 1)
+	statsd.Gauge("mem.stack_sys_bytes", float64(memstatsCurrent.StackSys), tags, 1)
 
 	// Collect number of bytes in use by mspan structures.
-	statsd.Gauge("mspan_inuse_bytes", float64(memstatsCurrent.MSpanInuse), tags, 1)
+	statsd.Gauge("mem.mspan_inuse_bytes", float64(memstatsCurrent.MSpanInuse), tags, 1)
 
 	// Collect number of bytes used for mspan structures obtained from system.
-	statsd.Gauge("mspan_sys_bytes", float64(memstatsCurrent.MSpanSys), tags, 1)
+	statsd.Gauge("mem.mspan_sys_bytes", float64(memstatsCurrent.MSpanSys), tags, 1)
 
 	// Collect number of bytes in use by mcache structures.
-	statsd.Gauge("mcache_inuse_bytes", float64(memstatsCurrent.MCacheInuse), tags, 1)
+	statsd.Gauge("mem.mcache_inuse_bytes", float64(memstatsCurrent.MCacheInuse), tags, 1)
 
 	// Collect number of bytes used for mcache structures obtained from system.
-	statsd.Gauge("mcache_sys_bytes", float64(memstatsCurrent.MCacheSys), tags, 1)
+	statsd.Gauge("mem.mcache_sys_bytes", float64(memstatsCurrent.MCacheSys), tags, 1)
 
 	// Collect number of bytes used by the profiling bucket hash table.
-	statsd.Gauge("buck_hash_sys_bytes", float64(memstatsCurrent.BuckHashSys), tags, 1)
+	statsd.Gauge("mem.buck_hash_sys_bytes", float64(memstatsCurrent.BuckHashSys), tags, 1)
 
 	// Collect number of bytes used for garbage collection system metadata.
-	statsd.Gauge("gc_sys_bytes", float64(memstatsCurrent.GCSys), tags, 1)
+	statsd.Gauge("mem.gc_sys_bytes", float64(memstatsCurrent.GCSys), tags, 1)
 
 	// Collect number of bytes used for other system allocations.
-	statsd.Gauge("other_sys_bytes", float64(memstatsCurrent.OtherSys), tags, 1)
+	statsd.Gauge("mem.other_sys_bytes", float64(memstatsCurrent.OtherSys), tags, 1)
 
 	// Collect number of heap bytes when next garbage collection will take pace.
-	statsd.Gauge("next_gc_bytes", float64(memstatsCurrent.NextGC), tags, 1)
+	statsd.Gauge("mem.next_gc_bytes", float64(memstatsCurrent.NextGC), tags, 1)
 }
