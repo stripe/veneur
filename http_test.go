b86033b05cfa78d73bb82f0aa717584f3ebecab1
Arnav Dugar <87779081+arnavdugar-stripe@users.noreply.github.com>
2022-08-29 08:59:54 -0700

    Remove unused ingestion logic. (#984)

--- http_test.go
+++ http_test.go
@@ -1,10 +1,6 @@
 package veneur
 
 import (
-	"bytes"
-	"compress/gzip"
-	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -12,237 +8,16 @@ import (
 	"net/http/httptest"
 	"os"
 	"path/filepath"
-	"sort"
 	"strconv"
 	"testing"
 	"time"
 
-	"github.com/stripe/veneur/v14/trace"
 	"github.com/stripe/veneur/v14/util"
 	"github.com/stripe/veneur/v14/util/build"
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
-	"github.com/stripe/veneur/v14/samplers"
 )
-
-func TestSortableJSONMetrics(t *testing.T) {
-	testList := []samplers.JSONMetric{{
-		MetricKey: samplers.MetricKey{Name: "foo", Type: "histogram"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "bar", Type: "set"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "baz", Type: "counter"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "qux", Type: "gauge"},
-	}}
-
-	sortable := newSortableJSONMetrics(testList, 96)
-	assert.EqualValues(t, []uint32{0x4f, 0x3a, 0x2, 0x3c}, sortable.workerIndices, "should have hashed correctly")
-
-	sort.Sort(sortable)
-	assert.EqualValues(t, []samplers.JSONMetric{{
-		MetricKey: samplers.MetricKey{Name: "baz", Type: "counter"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "bar", Type: "set"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "qux", Type: "gauge"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "foo", Type: "histogram"},
-	}}, testList, "should have sorted the metrics by hashes")
-}
-
-func TestSortableJSONMetricHashing(t *testing.T) {
-	packet, err := (&samplers.Parser{}).ParseMetric([]byte("foo:1|h|#bar"))
-	assert.NoError(t, err, "should have parsed test packet")
-
-	testList := []samplers.JSONMetric{{
-		MetricKey: packet.MetricKey,
-		Tags:      packet.Tags,
-	}}
-
-	sortable := newSortableJSONMetrics(testList, 96)
-	assert.Equal(t, 1, sortable.Len(), "should have exactly 1 metric")
-	assert.Equal(t, packet.Digest%96, sortable.workerIndices[0], "should have had the same hash")
-}
-
-func TestIteratingByWorker(t *testing.T) {
-	testList := []samplers.JSONMetric{{
-		MetricKey: samplers.MetricKey{Name: "foo", Type: "histogram"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "bar", Type: "set"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "foo", Type: "histogram"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "baz", Type: "counter"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "qux", Type: "gauge"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "baz", Type: "counter"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "bar", Type: "set"},
-	}, {
-		MetricKey: samplers.MetricKey{Name: "qux", Type: "gauge"},
-	}}
-
-	var testChunks [][]samplers.JSONMetric
-	iter := newJSONMetricsByWorker(testList, 96)
-	for iter.Next() {
-		nextChunk, workerIndex := iter.Chunk()
-		testChunks = append(testChunks, nextChunk)
-
-		for i := iter.currentStart; i < iter.nextStart; i++ {
-			assert.Equal(t, workerIndex, int(iter.sjm.workerIndices[i]), "mismatched worker index for %#v", iter.sjm.metrics[i])
-		}
-	}
-
-	assert.EqualValues(t, [][]samplers.JSONMetric{{
-		{MetricKey: samplers.MetricKey{Name: "baz", Type: "counter"}},
-		{MetricKey: samplers.MetricKey{Name: "baz", Type: "counter"}},
-	}, {
-		{MetricKey: samplers.MetricKey{Name: "bar", Type: "set"}},
-		{MetricKey: samplers.MetricKey{Name: "bar", Type: "set"}},
-	}, {
-		{MetricKey: samplers.MetricKey{Name: "qux", Type: "gauge"}},
-		{MetricKey: samplers.MetricKey{Name: "qux", Type: "gauge"}},
-	}, {
-		{MetricKey: samplers.MetricKey{Name: "foo", Type: "histogram"}},
-		{MetricKey: samplers.MetricKey{Name: "foo", Type: "histogram"}},
-	},
-	}, testChunks, "should have sorted the metrics by hashes")
-}
-
-func testServerImport(t *testing.T, filename string, contentEncoding string) {
-
-	f, err := os.Open(filename)
-	assert.NoError(t, err, "Error reading response fixture")
-	defer f.Close()
-
-	r := httptest.NewRequest(http.MethodPost, "/import", f)
-	r.Header.Set("Content-Encoding", contentEncoding)
-
-	w := httptest.NewRecorder()
-
-	config := localConfig()
-	s := setupVeneurServer(t, config, nil, nil, nil, nil)
-	defer s.Shutdown()
-
-	s.handleImport(w, r)
-
-	assert.Equal(t, http.StatusAccepted, w.Code, "Test server returned wrong HTTP response code")
-}
-
-func TestServerImportCompressed(t *testing.T) {
-	// Test that the global veneur instance can handle
-	// requests that provide compressed metrics
-	testServerImport(t, filepath.Join("testdata", "import.deflate"), "deflate")
-}
-
-func TestServerImportUncompressed(t *testing.T) {
-	// Test that the global veneur instance can handle
-	// requests that provide uncompressed metrics
-	testServerImport(t, filepath.Join("testdata", "import.uncompressed"), "")
-}
-
-func TestServerImportGzip(t *testing.T) {
-	// Test that the global veneur instance
-	// returns a 400 for gzipped-input
-
-	f, err := os.Open(filepath.Join("testdata", "import.uncompressed"))
-	assert.NoError(t, err, "Error reading response fixture")
-	defer f.Close()
-
-	var data bytes.Buffer
-	gz := gzip.NewWriter(&data)
-	_, err = io.Copy(gz, f)
-	assert.NoError(t, err)
-	gz.Close()
-
-	r := httptest.NewRequest(http.MethodPost, "/import", &data)
-	r.Header.Set("Content-Encoding", "gzip")
-
-	w := httptest.NewRecorder()
-
-	config := localConfig()
-	s := setupVeneurServer(t, config, nil, nil, nil, nil)
-	defer s.Shutdown()
-
-	s.handleImport(w, r)
-
-	assert.Equal(t, http.StatusUnsupportedMediaType, w.Code, "Test server returned wrong HTTP response code")
-}
-
-func TestServerImportCompressedInvalid(t *testing.T) {
-	// Test that the global veneur instance
-	// properly responds to invalid zlib-deflated data
-
-	//TODO(aditya) test that the metrics are properly reported
-
-	f, err := os.Open(filepath.Join("testdata", "import.uncompressed"))
-	assert.NoError(t, err, "Error reading response fixture")
-	defer f.Close()
-
-	r := httptest.NewRequest(http.MethodPost, "/import", f)
-	r.Header.Set("Content-Encoding", "deflate")
-
-	w := httptest.NewRecorder()
-
-	config := localConfig()
-	s := setupVeneurServer(t, config, nil, nil, nil, nil)
-	defer s.Shutdown()
-
-	s.handleImport(w, r)
-
-	assert.Equal(t, http.StatusBadRequest, w.Code, "Test server returned wrong HTTP response code")
-}
-
-func TestServerImportUncompressedInvalid(t *testing.T) {
-	// Test that the global veneur instance
-	// properly responds to invalid zlib-deflated data
-
-	//TODO(aditya) test that the metrics are properly reported
-
-	f, err := os.Open(filepath.Join("testdata", "import.deflate"))
-	assert.NoError(t, err, "Error reading response fixture")
-	defer f.Close()
-
-	r := httptest.NewRequest(http.MethodPost, "/import", f)
-	r.Header.Set("Content-Encoding", "")
-
-	w := httptest.NewRecorder()
-
-	config := localConfig()
-	s := setupVeneurServer(t, config, nil, nil, nil, nil)
-	defer s.Shutdown()
-
-	s.handleImport(w, r)
-
-	assert.Equal(t, http.StatusBadRequest, w.Code, "Test server returned wrong HTTP response code")
-}
-
-// TestServerImportEmptyError tests that the global
-// veneur instance returns an error
-// if it receives what amounts to an empty struct,
-// because that's usually the sign of an error
-func TestServerImportEmptyError(t *testing.T) {
-	// explicitly use the wrong type here
-	data := []struct {
-		Bad string
-	}{
-		{"Foo"},
-		{"Bar"},
-	}
-	testServerImportHelper(t, data)
-}
-
-// TestServerImportEmptyError tests that
-// the global veneur instance returns an error
-// if it receives an empty list, because the client
-// should never be sending an empty list.
-func TestServerImportEmptyListError(t *testing.T) {
-	data := []samplers.JSONMetric{}
-	testServerImportHelper(t, data)
-}
 
 func TestGeneralHealthCheck(t *testing.T) {
 	r := httptest.NewRequest(http.MethodGet, "/healthcheck", nil)
@@ -347,50 +122,6 @@ func TestVersion(t *testing.T) {
 	assert.NoError(t, err, "error reading /version")
 
 	assert.Equal(t, string(bts), build.VERSION, "received invalid version")
-}
-
-func testServerImportHelper(t *testing.T, data interface{}) {
-	var b bytes.Buffer
-	err := json.NewEncoder(&b).Encode(data)
-	assert.NoError(t, err)
-
-	r := httptest.NewRequest(http.MethodPost, "/import", &b)
-	r.Header.Set("Content-Encoding", "")
-
-	w := httptest.NewRecorder()
-
-	config := localConfig()
-	s := setupVeneurServer(t, config, nil, nil, nil, nil)
-	defer s.Shutdown()
-
-	s.handleImport(w, r)
-
-	assert.Equal(t, http.StatusBadRequest, w.Code, "Test server returned wrong HTTP response code")
-}
-
-func BenchmarkNewSortableJSONMetrics(b *testing.B) {
-	const numWorkers = 100
-	filename := filepath.Join("testdata", "import.deflate")
-	contentEncoding := "deflate"
-
-	f, err := os.Open(filename)
-	assert.NoError(b, err, "Error reading response fixture")
-	defer f.Close()
-
-	r := httptest.NewRequest(http.MethodPost, "/import", f)
-	r.Header.Set("Content-Encoding", contentEncoding)
-
-	w := httptest.NewRecorder()
-
-	logger := logrus.NewEntry(logrus.New())
-	_, jsonMetrics, err := unmarshalMetricsFromHTTP(
-		context.Background(), trace.DefaultClient, w, r, logger)
-	assert.NoError(b, err)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		newSortableJSONMetrics(jsonMetrics, numWorkers)
-	}
 }
 
 func TestConfigJson(t *testing.T) {
