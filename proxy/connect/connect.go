7f865f4fa4dd99bb710ba16dd92c008d2eeeddab
Arnav Dugar <arnavdugar@stripe.com>
2023-02-02 12:27:24 -0800

    Fix blocking write in veneur-proxy.

--- proxy/connect/connect.go
+++ proxy/connect/connect.go
@@ -45,8 +45,7 @@ type Destination interface {
 }
 
 type SendRequest struct {
-	Metric       *metricpb.Metric
-	ErrorChannel chan error
+	Metric *metricpb.Metric
 }
 
 type DestinationHash interface {
@@ -142,8 +141,6 @@ sendLoop:
 					"veneur_proxy.forward.metrics_count", 1,
 					[]string{"error:false"}, 1.0)
 			}
-			request.ErrorChannel <- err
-			close(request.ErrorChannel)
 		case <-ctx.Done():
 			break sendLoop
 		}
